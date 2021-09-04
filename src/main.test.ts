ab5a6170c10e0600ae8d46ccb76a4032bb480cf8
Michael Bøcker-Larsen <mbl@code.boutique>
2021-09-04 18:31:15 +0800

    feat: implement the bulk of the library

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,383 @@
-import { greet } from './main'
+import { create } from "./main";
 
-test('the data is peanut butter', () => {
-  expect(1).toBe(1)
+test("create > no now", () => {
+  const now = new Date();
+  const calendar = create();
+  expect(calendar.current.getFullYear()).toBe(now.getFullYear());
+  expect(calendar.current.getMonth()).toBe(now.getMonth());
+  expect(calendar.current.getDate()).toBe(now.getDate());
 });
 
-test('greeting', () => {
-  expect(greet('Foo')).toBe('Hello Foo')
+test("create > now as date", () => {
+  const now = new Date();
+  const calendar = create(now);
+  expect(calendar.current.getFullYear()).toBe(now.getFullYear());
+  expect(calendar.current.getMonth()).toBe(now.getMonth());
+  expect(calendar.current.getDate()).toBe(now.getDate());
+});
+
+test("create > now as date (fixed)", () => {
+  const now = new Date();
+  now.setDate(29);
+  now.setMonth(4);
+  now.setFullYear(2021);
+  const calendar = create(now);
+  expect(calendar.current.getFullYear()).toBe(now.getFullYear());
+  expect(calendar.current.getMonth()).toBe(now.getMonth());
+  expect(calendar.current.getDate()).toBe(now.getDate());
+});
+
+test("create > now as string", () => {
+  const calendar = create("2021-05-29");
+  expect(calendar.current.getFullYear()).toBe(2021);
+  expect(calendar.current.getMonth()).toBe(4);
+  expect(calendar.current.getDate()).toBe(29);
+});
+
+test("create > now as object", () => {
+  const calendar = create({
+    year: 2021,
+    month: 5,
+  });
+  expect(calendar.current.getFullYear()).toBe(2021);
+  expect(calendar.current.getMonth()).toBe(4);
+  expect(calendar.current.getDate()).toBe(1);
+});
+
+test("config defaults", () => {
+  const calendar = create();
+  expect(calendar.config).toStrictEqual({
+    after: undefined,
+    before: undefined,
+    fillWeek: true,
+    firstDay: 0,
+    otherDays: "show",
+    selection: undefined,
+  });
+});
+
+test("config simple", () => {
+  const calendar = create(new Date(), {
+    firstDay: 1,
+    fillWeek: false,
+    otherDays: "hide",
+  });
+  expect(calendar.config).toStrictEqual({
+    after: undefined,
+    before: undefined,
+    fillWeek: false,
+    firstDay: 1,
+    otherDays: "hide",
+    selection: undefined,
+  });
+});
+
+test("start monday", () => {
+  const calendar = create({ year: 2021, month: 9 }, { firstDay: 1 });
+
+  expect(calendar.days.map((day) => day.day)).toStrictEqual([
+    30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  ]);
+});
+
+test("start sunday", () => {
+  const calendar = create({ year: 2021, month: 9 });
+
+  expect(calendar.days.map((day) => day.day)).toStrictEqual([
+    29, 30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  ]);
+});
+
+test("start sunday on 1st", () => {
+  const calendar = create({ year: 2020, month: 11 });
+
+  expect(calendar.days.map((day) => day.day)).toStrictEqual([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  ]);
+});
+
+test("days after (start monday)", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, after: new Date(2021, 8, 5) }
+  );
+
+  expect(calendar.days.map((day) => day.state)).toStrictEqual([
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+  ]);
+});
+
+test("days before (start monday)", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, before: new Date(2021, 8, 28) }
+  );
+
+  expect(calendar.days.map((day) => day.state)).toStrictEqual([
+    "show",
+    "show",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+  ]);
+});
+
+test("days after and before (start monday)", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, after: new Date(2021, 8, 5), before: new Date(2021, 8, 28) }
+  );
+
+  expect(calendar.days.map((day) => day.state)).toStrictEqual([
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "active",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+    "show",
+  ]);
+});
+test("fillWeek off (start monday)", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { fillWeek: false, firstDay: 1 }
+  );
+
+  expect(calendar.days.map((day) => day.day)).toStrictEqual([
+    30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3,
+  ]);
+});
+
+test("fillWeek off (start sunday)", () => {
+  const calendar = create({ year: 2021, month: 9 }, { fillWeek: false });
+
+  expect(calendar.days.map((day) => day.day)).toStrictEqual([
+    29, 30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2,
+  ]);
+});
+
+test("selection > single", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, selection: [new Date(2021, 8, 2), new Date(2021, 8, 2)] }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual(["2-single"]);
+});
+
+test("selection > short", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, selection: [new Date(2021, 8, 2), new Date(2021, 8, 3)] }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual(["2-start", "3-end"]);
+});
+
+test("selection > long", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, selection: [new Date(2021, 8, 2), new Date(2021, 8, 5)] }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual(["2-start", "3-included", "4-included", "5-end"]);
+});
+
+test("selection > from previous", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    { firstDay: 1, selection: [new Date(2021, 7, 20), new Date(2021, 8, 2)] }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual(["30-included", "31-included", "1-included", "2-end"]);
+});
+
+test("selection > into next", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    {
+      fillWeek: false,
+      firstDay: 1,
+      selection: [new Date(2021, 8, 29), new Date(2021, 9, 20)],
+    }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual([
+    "29-start",
+    "30-included",
+    "1-included",
+    "2-included",
+    "3-included",
+  ]);
+});
+
+test("selection > a miss", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    {
+      fillWeek: false,
+      firstDay: 1,
+      selection: [new Date(2021, 9, 15), new Date(2021, 9, 16)],
+    }
+  );
+
+  expect(
+    calendar.days
+      .filter((day) => day.selection)
+      .map((day) => `${day.day}-${day.selection}`)
+  ).toStrictEqual([]);
+});
+
+test("selection > all included", () => {
+  const calendar = create(
+    { year: 2021, month: 9 },
+    {
+      fillWeek: false,
+      firstDay: 1,
+      selection: [new Date(2021, 7, 15), new Date(2021, 9, 16)],
+    }
+  );
+
+  expect(
+    calendar.days.filter((day) => day.selection).map((day) => day.selection)
+  ).toStrictEqual(Array(calendar.days.length).fill("included"));
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,253 @@
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+export type CalendarDayState = "hide" | "show" | "active";
 
-export const greet = (name: string): string => `Hello ${name}`
+export type CalendarSheet = {
+  config: CalendarBuilderConfig;
+  nextMonth: Date;
+  prevMonth: Date;
+  current: Date;
+  start: Date;
+  end: Date;
+  days: CalendarDay[];
+};
 
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
+export type CalendarDay = {
+  state: CalendarDayState;
+  day: number;
+  dayOfWeek: number;
+  selection: "start" | "end" | "included" | "single" | null;
+};
+
+type CalendarBuilderConfig = {
+  firstDay: number;
+  otherDays: CalendarDayState;
+  fillWeek: boolean;
+  after: Date | undefined;
+  before: Date | undefined;
+  selection: [Date, Date] | undefined;
+};
+
+export const create = (
+  now?:
+    | { year: number; month: number }
+    | string
+    | number
+    | Date
+    | null
+    | undefined,
+  options?: Partial<CalendarBuilderConfig>
+): CalendarSheet => {
+  const current = setMidnight(getCurrent(now));
+  if (!current || current.toString() === "Invalid Date") {
+    throw new Error("Invalid Date");
+  }
+  const start = new Date(current.getFullYear(), current.getMonth(), 1);
+  const end = new Date(current.getFullYear(), current.getMonth() + 1, 0);
+  const nextMonth = getNextMonth(current);
+  const prevMonth = getPrevMonth(current);
+
+  const config = createConfig(options);
+
+  const firstDay = start.getDay();
+
+  const weeks = config.fillWeek ? 6 : Math.ceil(end.getDate() / 7);
+
+  const firstDays = firstDay - config.firstDay;
+  const lastDays = weeks * 7 - (firstDays + end.getDate());
+
+  let indexOffset = 0;
+
+  const firstDaysFill = (
+    firstDays > 0 ? getDisplayDays(prevMonth).slice(-firstDays) : []
+  ).map((day, index) => {
+    const date = new Date(prevMonth.getFullYear(), prevMonth.getMonth(), day);
+    return {
+      day,
+      dayOfWeek: (index + indexOffset) % 7,
+      state: config.otherDays,
+      selection: getSelectionState(date, config.selection),
+    };
+  });
+
+  indexOffset += firstDaysFill.length;
+
+  const monthDaysFill = Array.from({ length: end.getDate() })
+    .map((_, index) => index + 1)
+    .map((day, index) => {
+      const date = new Date(current.getFullYear(), current.getMonth(), day);
+      return {
+        day,
+        dayOfWeek: (index + indexOffset) % 7,
+        state: clampDate(date, config.after, config.before),
+        selection: getSelectionState(date, config.selection),
+      };
+    });
+
+  indexOffset += monthDaysFill.length;
+
+  const lastDaysFill = getDisplayDays(nextMonth)
+    .slice(0, lastDays)
+    .map((day, index) => {
+      const date = new Date(nextMonth.getFullYear(), nextMonth.getMonth(), day);
+      return {
+        day,
+        dayOfWeek: (index + indexOffset) % 7,
+        state: config.otherDays,
+        selection: getSelectionState(date, config.selection),
+      };
+    });
+
+  const days: CalendarDay[] = [
+    ...firstDaysFill,
+    ...monthDaysFill,
+    ...lastDaysFill,
+  ];
+
+  // TODO selection
+
+  return {
+    config,
+    nextMonth,
+    prevMonth,
+    current,
+    start,
+    end,
+    days,
+  };
+};
+
+function setMidnight(date?: Date): Date | null | undefined {
+  if (!date) return;
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+}
+
+function getCurrent(
+  now?:
+    | { year: number; month: number }
+    | string
+    | number
+    | Date
+    | null
+    | undefined
+): Date {
+  if (now instanceof Date) return now;
+  if (typeof now === "number") return new Date(now);
+  if (typeof now === "string") return new Date(now);
+  if (
+    now &&
+    typeof now === "object" &&
+    typeof now.year === "number" &&
+    typeof now.month === "number"
+  ) {
+    return new Date(now.year, now.month - 1, 1, 0, 0, 0, 0);
+  }
+  return new Date();
+}
+
+function createConfig(
+  options?: Partial<CalendarBuilderConfig>
+): CalendarBuilderConfig {
+  return {
+    selection: options?.selection,
+    before: options?.before,
+    fillWeek: options?.fillWeek ?? true,
+    after: options?.after,
+    otherDays: options?.otherDays || "show",
+    firstDay: options?.firstDay || 0,
+  };
+}
+
+function getPrevMonth(current: Date): Date {
+  const prev = new Date(current);
+
+  if (current.getMonth() === 0) {
+    prev.setFullYear(prev.getFullYear() - 1);
+    prev.setMonth(11);
+  } else {
+    prev.setMonth(prev.getMonth() - 1);
+  }
+
+  return prev;
+}
+
+function getNextMonth(current: Date): Date {
+  const next = new Date(current);
+
+  if (current.getMonth() === 11) {
+    next.setFullYear(next.getFullYear() + 1);
+    next.setMonth(1);
+  } else {
+    next.setMonth(next.getMonth() + 1);
+  }
+
+  return next;
+}
+
+function getDisplayDays(current: Date): number[] {
+  const days = new Date(
+    current.getFullYear(),
+    current.getMonth() + 1,
+    0
+  ).getDate();
+  return Array.from({ length: days }).map((_, index: number) => index + 1);
+}
+
+function clampDate(
+  current: Date,
+  after: Date | undefined,
+  before: Date | undefined,
+  positiveState: CalendarDayState = "active",
+  negativeState: CalendarDayState = "show"
+): CalendarDayState {
+  if (!after && !before) return "active";
+
+  const val = current.getTime();
+
+  const min = after
+    ? new Date(
+        after.getFullYear(),
+        after.getMonth(),
+        after.getDate() + 1
+      ).getTime()
+    : 0;
+
+  const max = before
+    ? new Date(
+        before.getFullYear(),
+        before.getMonth(),
+        before.getDate() - 1
+      ).getTime()
+    : Number.MAX_SAFE_INTEGER;
+
+  const clampedDate = Math.min(Math.max(val, min), max);
+
+  return clampedDate === current.getTime() ? positiveState : negativeState;
+}
+
+function getSelectionState(
+  current: Date,
+  selection: CalendarBuilderConfig["selection"] | undefined
+): CalendarDay["selection"] {
+  const [from, to] = selection || [];
+  if (!from || !to) return null;
+
+  const min = setMidnight(from)?.getTime();
+  const val = current.getTime();
+  const max = setMidnight(to)?.getTime();
+
+  if (!min || !max) return null;
+
+  if (min === val && val === max) return "single";
+  if (min === val) return "start";
+  if (max === val) return "end";
+  if (min < val && val < max) return "included";
+
+  return null;
 }
